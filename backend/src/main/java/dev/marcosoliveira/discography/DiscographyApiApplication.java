ec4cac58a1ad75ca6f560893133a44c53e1a6751
Marcos Oliveira <ziguifrido@users.noreply.github.com>
2026-01-19 23:20:43 -0400

    config: Adding backend base structure

---
+++ backend/src/main/java/dev/marcosoliveira/discography/DiscographyApiApplication.java
@@ -0,0 +1,13 @@
+package dev.marcosoliveira.discography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscographyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscographyApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/marcosoliveira/discography/DiscographyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcosoliveira.discography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscographyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
